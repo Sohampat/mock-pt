655047545411d7f9e680f221eda360f6d9cadeab
Sohampat <110311523+Sohampat@users.noreply.github.com>
2024-01-12 22:16:46 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,65 @@
 namespace SpriteKind {
     export const Block = SpriteKind.create()
 }
+function CreateCave (col: number, row: number) {
+    range = 5
+    for (let Index2 = 0; Index2 <= range; Index2++) {
+        Chance = randint(0, 100)
+        if (Chance < 50) {
+            tiles.setTileAt(tiles.getTileLocation(col, row), assets.tile`transparency16`)
+        }
+    }
+}
 function UnderGroundColumn (Column: number, Row: number) {
     for (let i = 0; i <= Row; i++) {
         tiles.setTileAt(tiles.getTileLocation(Column, i + 7), Stone)
+        if (Math.percentChance(5)) {
+            tiles.setTileAt(tiles.getTileLocation(Column, i + 7), Iron)
+        }
+        if (Math.percentChance(10)) {
+            CreateCave(Column, i + 7)
+        }
     }
 }
 function doSomething () {
 	
 }
 function PlaceBlockColumn1 (Column: number, Row: number) {
-    for (let i = 0; i <= Row; i++) {
-        tiles.setTileAt(tiles.getTileLocation(Column, i * -1 + 7), dirt)
-        if (i == Row) {
-            tiles.setTileAt(tiles.getTileLocation(Column, i * -1 + 7 - 1), grass)
+    for (let j = 0; j <= Row; j++) {
+        tiles.setTileAt(tiles.getTileLocation(Column, j * -1 + 7), dirt)
+        if (j == Row) {
+            tiles.setTileAt(tiles.getTileLocation(Column, j * -1 + 7 - 1), grass)
         }
         if (Math.percentChance(5)) {
-            tiles.setTileAt(tiles.getTileLocation(Column, i * -1 + 7 - 2), tree)
+            tiles.setTileAt(tiles.getTileLocation(Column, j * -1 + 7 - 2), tree)
         }
     }
 }
 function GenerateTerrain () {
     current_tilemap_level = tilemap`level17`
     tiles.setCurrentTilemap(current_tilemap_level)
-    for (let index2 = 0; index2 <= 10000; index2++) {
+    for (let index22 = 0; index22 <= 10000; index22++) {
         DefaultBlockHeight = 2
         ColumBlockHeight = DefaultBlockHeight + randint(-2, 2) * randint(1, Noise)
-        PlaceBlockColumn1(index2, ColumBlockHeight)
-        UnderGroundColumn(index2, BlockHeightLimit)
+        PlaceBlockColumn1(index22, ColumBlockHeight)
+        UnderGroundColumn(index22, BlockHeightLimit)
     }
 }
 let ColumBlockHeight = 0
 let DefaultBlockHeight = 0
 let current_tilemap_level: tiles.TileMapData = null
+let Chance = 0
+let range = 0
+let Iron: Image = null
 let Stone: Image = null
 let tree: Image = null
 let dirt: Image = null
 let grass: Image = null
 let Noise = 0
 let BlockHeightLimit = 0
-BlockHeightLimit = 50
+let range2 = 0
 let col = 0
+BlockHeightLimit = 50
 Noise = 1
 grass = img`
     7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
@@ -97,22 +116,40 @@ tree = img`
     . . . . . e e e e e . . . . . . 
     `
 Stone = img`
-    f f d d d d d d d d d d d d f f 
-    f f d d d d f f f f d d d d f f 
-    d d f d d d d d d d d d d f d d 
-    d d d f d d d d d d d d f d d d 
-    d d d d f d d f f d d f d d d d 
-    d d d d d f d d d d f d d d d d 
-    d f d d d d f d d f d d d d f d 
-    d f d d f d d f f d d f d d f d 
-    d f d d f d d f f d d f d d f d 
-    d f d d d d f d d f d d d d f d 
-    d d d d d f d d d d f d d d d d 
-    d d d d f d d f f d d f d d d d 
-    d d d f d d d d d d d d f d d d 
-    d d f d d d d d d d d d d f d d 
-    f f d d d d f f f f d d d d f f 
-    f f d d d d d d d d d d d d f f 
+    f f b b b b b b b b b b b b f f 
+    f f b b f b b b b f b b b b f f 
+    b b f b b b b b b b b b b f b b 
+    b b b f b b b b b b b b f b b b 
+    b b b b f b b f b b b f b b b b 
+    b b b b b f b b b b f b b b b b 
+    b f b b b b f b b f b b b b b b 
+    b b b b b b b f f b b b f b b b 
+    b b b b b b b f f b b b b b b f 
+    b b b f f b f b b f b b b b b b 
+    b f b b b f b b b b f b b b b b 
+    b b b b f b b b b b b f b b b b 
+    b b b f b b b b b f b b f b b b 
+    b b f b f b b b b b b b b f b b 
+    f f b b b b b b f b b b b b f f 
+    f f b b b b b b b b b b b b f f 
+    `
+Iron = img`
+    b b d d d d d d d d d d d d b b 
+    b b d d b d d d d b d d d d b b 
+    d d b d d d d d d d d d d b d d 
+    d d d b d d d d d d d d b d d d 
+    d d d d b d d b d d d b d d d d 
+    d d d d d b d d d d b d d d d d 
+    d b d d d d b d d b d d d d d d 
+    d d d d d d d b b d d d b d d d 
+    d d d d d d d b b d d d d d d b 
+    d d d b b d b d d b d d d d d d 
+    d b d d d b d d d d b d d d d d 
+    d d d d b d d d d d d b d d d d 
+    d d d b d d d d d b d d b d d d 
+    d d b d b d d d d d d d d b d d 
+    b b d d d d d d b d d d d d b b 
+    b b d d d d d d d d d d d d b b 
     `
 let row = 7
 GenerateTerrain()
